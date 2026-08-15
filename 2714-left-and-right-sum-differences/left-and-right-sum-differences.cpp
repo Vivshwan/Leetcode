926ebc4257271c6f0c8b8b92f86e873c17154889
VKT <vivshwantomar17@gmail.com>
2026-08-15 18:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (16.98%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
 vector<int> left;
 vector<int> right;
-    void leftsum(vector<int>& nums){
+    void leftsum(vector<int>  &nums){
         int n = nums.size();
         left = vector<int> (n,0);
         for(int i =1 ; i<n;i++){
             left[i]=left[i-1]+nums[i-1];
         }
     }
-    void rightsum(vector<int>& nums){
+    void rightsum(vector<int> &nums){
         int n = nums.size();
         right = vector<int> (n,0);
         for(int i =n-2 ; i>=0;i--){
