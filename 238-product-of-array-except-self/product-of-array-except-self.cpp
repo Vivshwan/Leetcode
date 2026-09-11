769a2f5d5d9794827af21f4d7bb2e3801fbfa614
VKT <vivshwantomar17@gmail.com>
2026-09-12 03:38:33 +0530

    Time: 4 ms (21.01%) | Memory: 40.2 MB (80.87%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,8 +3,6 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n,1);
-        // vector<int> prefix(n,1);
-        // vector<int> suffix(n,1);
         
         for(int i = 1 ; i< n ; i++){
             ans[i]= ans[i-1]*nums[i-1];
