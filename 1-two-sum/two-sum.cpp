2a4314e526bf890b4beee96c41a53f22079da11d
VKT <vivshwantomar17@gmail.com>
2026-09-15 20:17:16 +0530

    Time: 36 ms (32.11%) | Memory: 14 MB (89.11%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        unordered_map<int, int> numMap;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            
-           
-            if (numMap.find(complement) != numMap.end()) {
-               
-                return {numMap[complement], i};
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
             }
-            
-           
-            numMap[nums[i]] = i;
         }
-        
         return {};
     }
 };
