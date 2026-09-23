238b19232be19a91d5b77f415537527744cc765b
VKT <vivshwantomar17@gmail.com>
2026-09-23 22:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.36%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long result = 0;  
+        
+        while (x != 0) {
+            result = result * 10 + (x % 10);
+            x /= 10;
+        }
+        
+        
+        if (result > INT_MAX || result < INT_MIN) {
+            return 0;
+        }
+        
+        return (int)result;
+    }
+};
